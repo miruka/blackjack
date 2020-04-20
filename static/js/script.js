702852184380cfd98bc77886822ab3e3c06fa8c3
miruka <xchrismiruka@gmail.com>
2020-04-20 17:28:55 +0300

    finished blackjack html

--- static/js/script.js
+++ static/js/script.js
@@ -139,11 +139,9 @@ function buttonsColorReset() {
 function randomColors() {
   let choices = ["btn-primary", "btn-danger", "btn-success", "btn-warning", "btn-info"];
   for (let i = 0; i < all_buttons.length; i++) {
-    let randomNumber = Math.floor(Math.random() * 5);
-
     all_buttons[i].classList.remove(all_buttons[i].classList[1]);
     all_buttons[i].classList.add(choices[randomNumber]);
   }
 }
 
-//How Blalckjack Works
+//How Blalckjack Workssssss
